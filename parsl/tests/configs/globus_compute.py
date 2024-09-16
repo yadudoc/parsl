dcc7feaf840b64d9388810b24ad1ac7ce291f999
Yadu Babuji <yadudoc1729@gmail.com>
2024-09-16 14:29:56 -0500

    Adding a github action for testing GlobusComputeExecutor

--- parsl/tests/configs/globus_compute.py
+++ parsl/tests/configs/globus_compute.py
@@ -6,8 +6,8 @@ from parsl.executors import GlobusComputeExecutor
 
 def fresh_config():
 
-    endpoint_id = os.environ.get("GLOBUS_COMPUTE_ENDPOINT",
-                                 "4b116d3c-1703-4f8f-9f6f-39921e5864df")
+    endpoint_id = os.environ.get("GLOBUS_COMPUTE_ENDPOINT")
+
     return Config(
         executors=[
             GlobusComputeExecutor(
